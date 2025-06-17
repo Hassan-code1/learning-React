6664e115c6cb27ed4c96aaef3ffe9b3bd2dbabf4
Hassan-code1 <2024kuec2011@iiitkota.ac.in>
2025-06-17 09:44:22 +0530

    Learning react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
 
+function MyForm(){
+  const [myCar, setMyCar] = useState("Volvo");
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+  }
+  return(
+    <form>
+      <select value={myCar} onChange={handleChange}>
+        <option value="Ford">Ford</option>
+        <option value="Audi">Volvo</option>
+        <option value="Mercedes">Fiat</option>
+      </select>
+    </form>
+  )
+}
+// const myFirstElement = (
+  
+//   // <div>//this is method 1 to put multiple html tags 
+//   //   <h1>pra1</h1>
+//   //   <h2>para2</h2>
+//   // </div>
+//   // <>
+//   //    <h1>para</h1>
+//   //    <h2>para1</h2>
+//   // </>
+// )
+
+
+// function MyForm(){
+  
+//   const [textarea, setTextarea] = useState(
+//     "wow! A text area"
+//   );
+//   const handleChange = (event) => {
+//     setTextarea(event.target.value)
+//   }
+
+//   return (
+//     // <h1>Hello</h1>
+//     <form>
+//       <textarea value={textarea} onChange={handleChange} />
+//     </form>
+//   )
+// }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<MyForm />)
