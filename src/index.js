8bef50a4abfb41a8142008f8038650f391de9763
Hassan-code1 <2024kuec2011@iiitkota.ac.in>
2025-06-17 19:16:34 +0530

    Completed one module on alison

--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
-import React from "react";
 import ReactDOM from 'react-dom/client';
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import Todos from "./Todos";
 
-function MyForm(){
-  const [myCar, setMyCar] = useState("Volvo");
-  const handleChange = (event) => {
-    setMyCar(event.target.value)
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+  const incrment = () => {
+    setCount((c) => c+1);
+  };
+
   return(
-    <form>
-      <select value={myCar} onChange={handleChange}>
-        <option value="Ford">Ford</option>
-        <option value="Audi">Volvo</option>
-        <option value="Mercedes">Fiat</option>
-      </select>
-    </form>
-  )
-}
+    <>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count : {count}
+        <button onClick={incrment}>+</button>
+      </div>
+    </>
+  );
+};
+
+// function MyForm(){
+//   const [myCar, setMyCar] = useState("Volvo");
+//   const handleChange = (event) => {
+//     setMyCar(event.target.value)
+//   }
+//   return(
+//     <form>
+//       <select value={myCar} onChange={handleChange}>
+//         <option value="Ford">Ford</option>
+//         <option value="Audi">Volvo</option>
+//         <option value="Mercedes">Fiat</option>
+//       </select>
+//     </form>
+//   )
+// }
 // const myFirstElement = (
   
 //   // <div>//this is method 1 to put multiple html tags 
@@ -47,4 +82,4 @@ function MyForm(){
 //   )
 // }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />)
+root.render(<App />)
